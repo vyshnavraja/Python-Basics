1fe3ab1675c75d3688a746ecc247135feda82838
Vyshnav <68264268+vyshnavraja@users.noreply.github.com>
2024-03-24 20:26:19 +0530

    Create Quadratic Equation Solver with Complex Roots in Python 1.py

---
+++ Quadratic Equation Solver with Complex Roots in Python 1.py
@@ -0,0 +1,15 @@
+import cmath
+
+a=int(input("Enter 1st number"))
+print(a)
+b=int(input("Enter 2nd number"))
+print(b)
+c=int(input("Enter 3rd number"))
+print(c)
+d=(-b**2)-(4*a*c)
+print(d)
+
+'''res2 = ( - b - cmath.sqrt(d)) / (2 * a)
+print = (res2)'''
+res3 = ( - b + cmath.sqrt(d)) / (2 * a)
+print = (res3)
